956245e5dc1480ea7ac7d70cdf292a54a601df96
justin-wenning <wenning.justin@gmail.com>
2020-02-18 16:48:20 -0800

    Completed Drills01.

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,16 +1,35 @@
 
-function sumArray(){
-	
+function sumArray(arr){
+	return arr.reduce((a, b) => a + b);
 }
 
-function fitWithinVal(){
-
+function fitWithinVal(arr, val){
+  const nums = [];
+  let total = 0;
+  for(let i = 0; i < arr.length; ++i)
+    if(arr[i] + total <= val) {
+      nums.push(arr[i]);
+      total += arr[i];
+    }
+  return nums;
 }
 
-function getAllNamesShorterThan(){
-
+function getAllNamesShorterThan(arr, val){
+  return arr.filter(a=> a.length < val);
 }
 
-function makeLabel(){
-
+function makeLabel(obj){
+  const {
+    familyName,
+    givenName,
+    greeting,
+    "home address":{
+      streetNumber,
+      streetName,
+      state,
+      zip,
+      city
+    }
+  } = obj;
+  return `${greeting} ${givenName} ${familyName}\n${streetNumber} ${streetName}\n${city}, ${state} ${zip}`;
 }
